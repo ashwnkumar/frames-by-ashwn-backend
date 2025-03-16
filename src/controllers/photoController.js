955ce3324ae78f1411b15332ca3706ace009337f
ashwnkumar <ashwin12kumar07@gmail.com>
2025-03-16 11:39:50 +0530

    api changes?

--- src/controllers/photoController.js
+++ src/controllers/photoController.js
@@ -4,22 +4,38 @@ const { sendResponse } = require("../utils/sendResponse");
 
 const uploadPhoto = async (req, res) => {
   try {
-    const { title, description, category } = req.body;
+    const { title, description, album } = req.body;
+    console.log("req.file from add", req.file);
+
     if (!req.file) {
       return sendResponse(res, 400, "Photo is required");
     }
-
     const result = await cloudinary.uploader.upload(req.file.path, {
       folder: "frames-by-ashwn",
       use_filename: true,
+      transformation: [{ quality: "auto", fetch_format: "auto" }],
+      eager: [
+        { width: 500, quality: "auto", fetch_format: "auto" }, // Preview Image
+      ],
     });
+
+    const image = {
+      fullSizeUrl: result.secure_url, // Original Image
+      previewUrl: result.eager[0].secure_url, // Optimized Preview Image
+    };
+
+    // Save `image.fullSizeUrl` and `image.previewUrl` in your database
 
     const uploadedPhoto = new Photo({
       title,
       description,
-      imageUrl: result.secure_url,
-      category,
+      imageUrl: result.secure_url, // Original Image
+      previewUrl: result.eager[0].secure_url, // Optimized Preview Image
+      album,
       publicId: result.public_id,
+      width: result.width,
+      height: result.height,
+      aspectRatio: result.width / result.height,
     });
 
     await uploadedPhoto.save();
@@ -33,7 +49,7 @@ const uploadPhoto = async (req, res) => {
 const getPhotos = async (req, res) => {
   try {
     const photo = await Photo.find().sort({ createdAt: -1 });
-    sendResponse(res, 200, "Photos fetched", { photo });
+    sendResponse(res, 200, "Photos fetched", { photos: photo });
   } catch (error) {
     sendResponse(res, 500, "Error fetching photos", { error: error.message });
     console.error("Error fetching photos", error);
@@ -72,7 +88,8 @@ const deletePhoto = async (req, res) => {
 
 const updatePhoto = async (req, res) => {
   try {
-    console.log("req", req);
+    console.log("req from update", req.body);
+    console.log("req from update", req.file);
     const { title, description, category } = req.body;
     const photo = await Photo.findById(req.params.id);
     if (!photo) {
@@ -84,15 +101,27 @@ const updatePhoto = async (req, res) => {
       const result = await cloudinary.uploader.upload(req.file.path, {
         folder: "frames-by-ashwn",
         use_filename: true,
+        transformation: [{ quality: "auto", fetch_format: "auto" }],
+        eager: [
+          { width: 500, quality: "auto", fetch_format: "auto" }, // Preview Image
+        ],
       });
+
+      console.log("result from update", result);
 
       photo.imageUrl = result.secure_url;
       photo.publicId = result.public_id;
+      photo.width = result.width;
+      photo.height = result.height;
+      photo.aspectRatio = result.width / result.height;
+      photo.previewUrl = result.eager[0].secure_url;
     }
 
     photo.title = title || photo.title;
     photo.description = description || photo.description;
     photo.category = category || photo.category;
+
+    console.log("photo after save", photo);
 
     await photo.save();
     sendResponse(res, 200, "Photo updated", { photo });
--- src/models/photoModel.js
+++ src/models/photoModel.js
@@ -3,10 +3,15 @@ const { default: mongoose } = require("mongoose");
 const photoSchema = mongoose.Schema(
   {
     title: { type: String, required: true },
-    description: { type: String, required: true },
     imageUrl: { type: String, required: true },
+    description: { type: String },
     category: { type: String },
     publicId: { type: String },
+    width: { type: Number },
+    height: { type: Number },
+    aspectRatio: { type: Number },
+    imageUrl: { type: String },
+    previewUrl: { type: String },
   },
   { timestamps: true }
 );
